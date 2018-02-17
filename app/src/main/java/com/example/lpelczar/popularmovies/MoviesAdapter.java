e959a06335357d6fb6676e5f1e2e8200e58da178
lpelczar <lukepelczar@gmail.com>
2018-02-17 20:04:11 +0100

    Implement on create view holder method

--- app/src/main/java/com/example/lpelczar/popularmovies/MoviesAdapter.java
+++ app/src/main/java/com/example/lpelczar/popularmovies/MoviesAdapter.java
@@ -3,6 +3,9 @@ package com.example.lpelczar.popularmovies;
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
 import com.example.lpelczar.popularmovies.models.Movie;
 
 import java.util.ArrayList;
@@ -22,5 +25,22 @@ public class MoviesAdapter extends RecyclerView.Adapter<MovieViewHolder>  {
         this.movieList = new ArrayList<>();
         this.inflater = LayoutInflater.from(context);
         this.context = context;
+    }
+
+
+    @Override
+    public MovieViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = inflater.inflate(R.layout.movie_item, parent, false);
+        return new MovieViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(MovieViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
     }
 }
