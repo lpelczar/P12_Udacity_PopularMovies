7b19d92a9866958fa1e059e682ba2668af87e827
lpelczar <lukepelczar@gmail.com>
2018-02-21 20:09:44 +0100

    Implement displaying average vote and plot synopsis

--- app/src/main/java/com/example/lpelczar/popularmovies/DetailActivity.java
+++ app/src/main/java/com/example/lpelczar/popularmovies/DetailActivity.java
@@ -15,6 +15,7 @@ import com.example.lpelczar.popularmovies.models.Movie;
 import com.squareup.picasso.Picasso;
 
 import java.util.List;
+import java.util.Locale;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -46,6 +47,13 @@ public class DetailActivity extends AppCompatActivity {
 
         TextView title = findViewById(R.id.title_tv);
         title.setText(movie.getTitle());
+        
+        TextView averageVote = findViewById(R.id.average_vote_tv);
+        averageVote.setText(String.format(Locale.getDefault(),
+                "%.2f", movie.getAverageVote()));
+
+        TextView description = findViewById(R.id.description_tv);
+        description.setText(movie.getPlot());
     }
 
     private void closeOnError() {
