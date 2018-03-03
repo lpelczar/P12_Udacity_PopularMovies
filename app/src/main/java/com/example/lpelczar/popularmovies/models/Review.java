cd3d9d53b301dcc29ae18f86873e60858221417b
lpelczar <lukepelczar@gmail.com>
2018-03-03 12:23:28 +0100

    Add review model

---
+++ app/src/main/java/com/example/lpelczar/popularmovies/models/Review.java
@@ -0,0 +1,59 @@
+package com.example.lpelczar.popularmovies.models;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+/**
+ * Created by lpelczar on 03.03.18.
+ */
+
+public class Review implements Parcelable {
+
+    private String author;
+    private String content;
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public static final Parcelable.Creator<Review> CREATOR = new Parcelable.Creator<Review>() {
+        public Review createFromParcel(Parcel in) {
+            return new Review(in);
+        }
+
+        public Review[] newArray(int size) {
+            return new Review[size];
+        }
+    };
+
+    // Parcelling part
+    Review(Parcel in){
+        this.author = in.readString();
+        this.content = in.readString();
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(this.author);
+        dest.writeString(this.content);
+    }
+
+
+}
